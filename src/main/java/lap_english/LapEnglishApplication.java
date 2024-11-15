ff67ce26bac90b845fdac1f0de810ac437654b9c
ta dang thanh <tathanh.mycv@gmail.com>
2024-11-15 18:38:37 +0700

    u[pdatE

--- src/main/java/lap_english/LapEnglishApplication.java
+++ src/main/java/lap_english/LapEnglishApplication.java
@@ -1,5 +1,6 @@
 package lap_english;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LapEnglishApplication {
 
 	public static void main(String[] args) {
+		// Load .env variables into environment
+		Dotenv dotenv = Dotenv.configure().load();
+		System.setProperty("AZURE_STORAGE_CONNECTION_STRING", dotenv.get("AZURE_STORAGE_CONNECTION_STRING"));
 		SpringApplication.run(LapEnglishApplication.class, args);
 	}
 
