bd75fb3f8c52c06f3d905182229512bd58ec840a
tadangthanh <tathanh.mycv@gmail.com>
2024-12-16 15:15:19 +0700

    diuasd

--- src/main/java/lap_english/repository/UserLearnedSubTopicRepo.java
+++ src/main/java/lap_english/repository/UserLearnedSubTopicRepo.java
@@ -2,6 +2,7 @@ package lap_english.repository;
 
 import lap_english.entity.UserLearnedSubTopic;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -11,5 +12,6 @@ public interface UserLearnedSubTopicRepo extends JpaRepository<UserLearnedSubTop
 
     Optional<UserLearnedSubTopic> findByUserIdAndSubTopicId(Long userId, Long subTopicId);
     boolean existsBySubTopicId(Long subTopicId);
+    @Query("select count(u) from UserLearnedSubTopic u where u.user.id = ?1 and u.subTopic.id = ?2")
     boolean existByUserIdAndSubtopicId(Long userId, Long subTopicId);
 }
