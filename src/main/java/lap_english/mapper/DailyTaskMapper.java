e87ef25c753141c5634d8f3dbb645614aa751d48
tadangthanh <tathanh.mycv@gmail.com>
2024-12-17 21:03:07 +0700

    dasd

--- src/main/java/lap_english/mapper/DailyTaskMapper.java
+++ src/main/java/lap_english/mapper/DailyTaskMapper.java
@@ -3,11 +3,13 @@ package lap_english.mapper;
 import lap_english.dto.DailyTaskDto;
 import lap_english.entity.DailyTask;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import static org.mapstruct.NullValuePropertyMappingStrategy.IGNORE;
 
 @Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = IGNORE)
 public interface DailyTaskMapper {
+    @Mapping(target = "id", source = "id")
     DailyTaskDto toDto(DailyTask dailyTask);
     DailyTask toEntity(DailyTaskDto dailyTaskDto);
 }
